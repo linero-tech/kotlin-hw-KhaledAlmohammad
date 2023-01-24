93e08ca1fa8b4789598620b040ef5ff5e84c4331
KhaledAlmohammad <122489320+KhaledAlmohammad@users.noreply.github.com>
2023-01-24 22:06:13 +0100

    solution task 3 module 101

--- src/main/kotlin/lms_126/Task3.kt
+++ src/main/kotlin/lms_126/Task3.kt
@@ -3,9 +3,9 @@ package lms_126
 fun task3(): String {
 
     return """
-        INPUT height of triangle
-        INPUT base of triangle
+        val height of triangle
+        val base of triangle
         Calculate area = 0.5 * base * height
-        Print area of triangle
+        return result
     """
 }
