0ae0bc3bf8faacd9f8e3359fb994bc6c490ec7ae
KhaledAlmohammad <122489320+KhaledAlmohammad@users.noreply.github.com>
2023-01-26 22:41:36 +0100

    Solution module 102 Task 3+4

--- src/main/kotlin/lms_127/Task3.kt
+++ src/main/kotlin/lms_127/Task3.kt
@@ -3,9 +3,9 @@ package lms_127
 fun task3(radius: Double): Double {
     return
 
-    val radius = 2.0
-    val pi = 3.1416
-    val perimeterOfCircle = 2 * pi * radius
+    val radius= 2.0
+    val pi= 3.1416
+    val perimeterOfCircle= 2 * pi * radius
     println("result is $perimeterOfCircle")
 }
 fun main() {
--- src/main/kotlin/lms_127/Task4.kt
+++ src/main/kotlin/lms_127/Task4.kt
@@ -2,5 +2,13 @@ package lms_127
 
 fun task4(base: Double, height: Double): Double {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    return
+    val base = 5.0
+    val height = 10.0
+    val areaOfTriangle = 1/2 * base * height
+    println("result is $areaOfTriangle")
+
+}
+fun main() {
+    task4()
 }
