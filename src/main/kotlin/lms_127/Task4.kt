43c59bb878b17cdf0b978f42242d3e0a4155de78
KhaledAlmohammad <122489320+KhaledAlmohammad@users.noreply.github.com>
2023-01-29 22:07:31 +0100

    Solution Module 102 Task 4

--- src/main/kotlin/lms_127/Task4.kt
+++ src/main/kotlin/lms_127/Task4.kt
@@ -2,11 +2,11 @@ package lms_127
 
 fun task4(base: Double, height: Double): Double {
 
-    val result = 1/2 * base * height
+    val result = 0.5 * base * height
     return result
 
 }
 fun main() {
-    val answer = task4(base = 5.0, 10.0)
+    val answer = task4(base = 5.0, height = 10.0)
     println (answer)
 }
