8108b97a9d592854c2b8119a361f309c0772d124
KhaledAlmohammad <122489320+KhaledAlmohammad@users.noreply.github.com>
2023-01-29 21:51:34 +0100

    Solution Module 102 Task 4

--- src/main/kotlin/lms_127/Task4.kt
+++ src/main/kotlin/lms_127/Task4.kt
@@ -2,13 +2,11 @@ package lms_127
 
 fun task4(base: Double, height: Double): Double {
 
-    return
-    val base = 5.0
-    val height = 10.0
-    val areaOfTriangle = 1/2 * base * height
-    println("result is $areaOfTriangle")
+    val result = 1/2 * base * height
+    return result
 
 }
 fun main() {
-    task4()
+    val answer = task4(base = 5.0, 10.0)
+    println (answer)
 }
