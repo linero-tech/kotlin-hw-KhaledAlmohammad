121001cb874307fef024a62e2506fc7ac3b2697a
KhaledAlmohammad <122489320+KhaledAlmohammad@users.noreply.github.com>
2023-01-31 12:19:25 +0100

    Solution Module 101 Task 10

--- src/main/kotlin/lms_126/Task10.kt
+++ src/main/kotlin/lms_126/Task10.kt
@@ -2,5 +2,13 @@ package lms_126
 
 fun task10(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        INPUT "create password"
+        Test the user's password against the six characters long.
+        store the password
+        IF the password does not match the rule of the six characters long.
+        OUTPUT "Invalid password, the password have to include six character long"
+        ELSE
+        OUTPUT " The account created successfully"
+    """.trimIndent()
 }
