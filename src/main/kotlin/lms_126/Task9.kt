0aa4f8958c90a2fc738880e1474d5bcadd9e69d1
KhaledAlmohammad <122489320+KhaledAlmohammad@users.noreply.github.com>
2023-01-31 10:54:31 +0100

    Solution Module 101 Task 9

--- src/main/kotlin/lms_126/Task9.kt
+++ src/main/kotlin/lms_126/Task9.kt
@@ -2,5 +2,17 @@ package lms_126
 
 fun task9(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        OUTPUT "select option"
+        INPUT withdraw
+        OUTPUT " enter amount to withdraw"
+        INPUT amount
+        OUTPUT "confirm the amount is correct"
+        INPUT yes
+        OUTPUT " transaction is in process"
+        OUTPUT " take your money"
+        OUTPUT " press Enter to end Or back to do another process"
+        
+        
+    """.trimIndent()
 }
