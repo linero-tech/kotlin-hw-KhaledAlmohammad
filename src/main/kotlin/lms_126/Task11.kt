935d5b0f36e46928ca7d764f59a057fefa157f4c
KhaledAlmohammad <122489320+KhaledAlmohammad@users.noreply.github.com>
2023-01-31 12:43:27 +0100

    Solution Module 101 Task 11

--- src/main/kotlin/lms_126/Task11.kt
+++ src/main/kotlin/lms_126/Task11.kt
@@ -2,5 +2,13 @@ package lms_126
 
 fun task11(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        INPUT the temperature 
+        IF the input contain C
+        Convert to Fahrenheit by use the calculation F= 9/5 * C + 32
+        OUTPUT F
+        ELSE, IF the input contain F
+        Convert to Celsius by use the calculation C=(F-32)*5/9
+        OUTPUT C
+    """.trimIndent()
 }
