e4e3f225dad5aa3a2ade9a44240d8fe5828f572e
KhaledAlmohammad <122489320+KhaledAlmohammad@users.noreply.github.com>
2023-01-31 09:49:35 +0100

    Solution Module 101 Task 7

--- src/main/kotlin/lms_126/Task7.kt
+++ src/main/kotlin/lms_126/Task7.kt
@@ -2,5 +2,9 @@ package lms_126
 
 fun task7(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        For number is 1 to 100
+        If number is even
+        Output number
+    """.trimIndent()
 }
