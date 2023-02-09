342e4174a6b7f38cdf87932228159e28a45a0c8e
KhaledAlmohammad <122489320+KhaledAlmohammad@users.noreply.github.com>
2023-02-09 14:49:49 +0100

    Solution 102 Task 9

--- src/main/kotlin/lms_127/Task9.kt
+++ src/main/kotlin/lms_127/Task9.kt
@@ -1,6 +1,11 @@
 package lms_127
 
 fun task9(sentence: String, character: Char): Boolean {
+    val result = sentence.contains(character, ignoreCase = true)
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    return result
+}
+fun main () {
+    val answer = task9("I code in KOTLIN", 'i')
+    println("result is $answer")
 }
