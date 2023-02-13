3e2233e63df5877924e7df29122fb769f99a9149
KhaledAlmohammad <122489320+KhaledAlmohammad@users.noreply.github.com>
2023-02-13 12:33:29 +0100

    Solution 102 Task 6

--- src/main/kotlin/lms_127/Task6.kt
+++ src/main/kotlin/lms_127/Task6.kt
@@ -6,5 +6,5 @@ fun task6(base1: Double, base2: Double, height: Double): Double {
 }
 fun main() {
     val answer = task6(base1 = 10.0, base2 = 20.0, height = 1.0)
-    println(answer)
+    println("$answer")
 }
