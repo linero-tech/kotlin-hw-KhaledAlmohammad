1ddafc62baa34c39b9c271939acb123118f4b92f
KhaledAlmohammad <122489320+KhaledAlmohammad@users.noreply.github.com>
2023-02-21 15:18:07 +0100

    Solution 103 task 2

--- src/main/kotlin/lms_129/Task2.kt
+++ src/main/kotlin/lms_129/Task2.kt
@@ -1,6 +1,10 @@
 package lms_129
 
 fun task2(number: Int): Boolean {
-
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val result = number > 1 && number < 100
+    return result
+}
+fun main(){
+    val answer = task2(5)
+    println("$answer")
 }
