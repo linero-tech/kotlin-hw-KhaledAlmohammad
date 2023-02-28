47bd9105d1b7c6c4ce31446f8e2fb0c2e17ed552
KhaledAlmohammad <122489320+KhaledAlmohammad@users.noreply.github.com>
2023-02-28 11:37:06 +0100

    103 task 4

--- src/main/kotlin/lms_129/Task4.kt
+++ src/main/kotlin/lms_129/Task4.kt
@@ -1,12 +1,13 @@
 package lms_129
 
+
+
 fun task4(): Int {
     var result = 0
     for (number in 1..1000){
         if (number % 9 == 0)
-            result = number
+            result+= number
     }
-
     return result
 
 }
