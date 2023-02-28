f107fba800e522030cf9184ea89da3883a088252
KhaledAlmohammad <122489320+KhaledAlmohammad@users.noreply.github.com>
2023-02-28 12:36:36 +0100

    103 task 2

--- src/main/kotlin/lms_129/Task2.kt
+++ src/main/kotlin/lms_129/Task2.kt
@@ -1,10 +1,10 @@
 package lms_129
 
 fun task2(number: Int): Boolean {
-    val result = number > 1 && number < 100
+    val result = number > 1 && number % number ==0 && number % 1 == 0 && number % 2 != 0
     return result
 }
 fun main(){
-    val answer = task2(5)
+    val answer = task2(4)
     println("$answer")
 }
