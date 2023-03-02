7737cf3a528aefb5a163e9122751443a5debbb3c
KhaledAlmohammad <122489320+KhaledAlmohammad@users.noreply.github.com>
2023-03-02 08:41:04 +0100

    103 _5

--- src/main/kotlin/lms_129/Task5.kt
+++ src/main/kotlin/lms_129/Task5.kt
@@ -1,6 +1,14 @@
 package lms_129
 
 fun task5(sentence: String): Int {
+var result = 0
+    for (i in sentence){
+        result = sentence.length
+    }
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+
+    return result
+}
+fun main(){
+    println(task5("I love GBG"))
 }
