ec657ff2ebe53899550202ec8f5351dff3919561
KhaledAlmohammad <122489320+KhaledAlmohammad@users.noreply.github.com>
2023-03-03 11:20:33 +0100

    103 _ 8

--- src/main/kotlin/lms_129/Task8.kt
+++ src/main/kotlin/lms_129/Task8.kt
@@ -1,6 +1,14 @@
 package lms_129
 
 fun task8(number: Int): Int {
-
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var result = 0
+    var i = number
+    while (i > 0){
+        result += i % 10
+        i /= 10
+    }
+    return result
+}
+fun main(){
+    println(task8(66))
 }
