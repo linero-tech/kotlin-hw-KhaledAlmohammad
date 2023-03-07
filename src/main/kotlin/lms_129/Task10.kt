5f13c154fc0dd1bc7b02df6e9608e2a3c195ab71
KhaledAlmohammad <122489320+KhaledAlmohammad@users.noreply.github.com>
2023-03-07 14:58:43 +0100

    103_10 update

--- src/main/kotlin/lms_129/Task10.kt
+++ src/main/kotlin/lms_129/Task10.kt
@@ -3,9 +3,30 @@ package lms_129
 
 fun task10(password: String): Boolean {
 
-val result = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$#@]{6,10}\$)".toRegex()
+//val result = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$#@]{6,10}\$)".toRegex()
+    //result.matches(password)
+    if (password.length < 6 || password.length > 10){
+        return false
 
-    return result.matches(password)
+    }
+    var includeNumber = false
+    var includeLowCase = false
+    var includeUpperCase = false
+    var includeSpecial = false
+
+    for (i in password){
+        if (i.isLowerCase()){
+            includeLowCase = true
+        } else if (i.isDigit()){
+            includeNumber = true
+        }else if (i.isUpperCase()){
+            includeUpperCase = true
+        } else if (!i.isLetterOrDigit()){
+            includeSpecial =true
+        }
+    }
+
+    return includeLowCase && includeUpperCase && includeNumber && includeSpecial
 }
 fun main(){
     println(task10("Test@2pass"))
